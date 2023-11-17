94d4b539ae46ebd7648d31e54aee4db2c3fe3fa5
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-17 21:24:23 +0700

    feat: add lesson 3

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://admin:admin@cluster1.zpi8q.mongodb.net/bookstore-db?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.get("/", (req, res) => {
   return res.status(234).send("Welcome to MERN Stack tutorial");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
