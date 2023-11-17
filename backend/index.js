7db0774ff1564c6f79a06bcd147f512f6fcf65a4
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-17 21:07:10 +0700

    feat: add lesson 2

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  console.log(res);
+  return res.status(234).send("Welcome to MERN Stack tutorial");
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
 });
