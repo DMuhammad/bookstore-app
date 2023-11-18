29a2f4664cb31a7f72f806c2b583d425a7ef2398
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-18 11:24:56 +0700

    feat: add lesson 5

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,39 @@
 import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
+import { Book } from "./models/BookModel.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(res);
   return res.status(234).send("Welcome to MERN Stack tutorial");
+});
+
+app.post("/book", async (req, res) => {
+  try {
+    const { title, author, publishYear } = req.body;
+
+    if (!title || !author || !publishYear) {
+      return res
+        .status(401)
+        .send({ message: "Send all fields: title, author, publishYear" });
+    }
+
+    const newBook = {
+      title,
+      author,
+      publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
