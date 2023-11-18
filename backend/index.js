b846759f81ee954b6a90efe504ab1b8578efdb52
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-18 12:14:13 +0700

    feat: add lesson 11

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { PORT, mongoDBURL } from "./config.js";
 import bookRouter from "./routes/booksRoute.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   console.log(res);
