ecd7d98593d0cf86015fb4f7be7c43c379d9ec9f
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-18 11:30:16 +0700

    feat: add lesson 6

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,19 @@ app.post("/book", async (req, res) => {
   }
 });
 
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
